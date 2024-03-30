c6e8606c88d88ddbff823febc0448d288bf6f9a7
V. hs Sin <cbrnt1210@naver.com>
2024-03-30 16:11:24 +0900

    단어 test 할 수 있게 함

--- word_data_trans/excel_to_pandas.py
+++ word_data_trans/excel_to_pandas.py
@@ -1,21 +1,36 @@
 import pandas as pd
-path = 'C:\\Users\\cbrnt\\Desktop\\영단어.xlsx'
+import os
 
-# 엑셀 파일 읽기
-excel_data = pd.read_excel(path, usecols=range(5), header=0).dropna() #결측치 행은 패스
 
-# 데이터 확인
-# print(excel_data.tail())
-# print(excel_data.shape)
+def add_data(new_data_path):
+    # 엑셀 파일 읽기
+    excel_data = pd.read_excel(new_data_path, usecols=range(5), header=0).dropna() #결측치 행은 패스
 
-#초기 가중치는 100
-value = [100] * excel_data.shape[0]
-excel_data.insert(0, "value", value)
-# print(excel_data.tail())
+    # 데이터 확인
+    # print(excel_data.tail(10))
+    # print(excel_data.shape)
 
-from datetime import datetime
+    #초기 가중치는 0
+    value = [0] * excel_data.shape[0]
+    excel_data.insert(0, "value", value)
+    # print(excel_data.tail(10))
 
-# 현재 날짜 및 시간 가져와 데이터 생성
-today = datetime.now().strftime('%Y-%m-%d')
+    data_path = f'word_data\\end_word\\word_data.csv'
+    if not os.path.exists(data_path):
+        excel_data.to_csv(data_path, index=False)
+    else:
+        existing_data = pd.read_csv(data_path)
+        last_index = existing_data.index[-1]+1 if not existing_data.empty else 0 #1부터 시작함!
+        # print(last_index)
+        # print(existing_data.tail(10))
+        excel_data["인덱스"] += last_index
+        # print(excel_data.head(10))
+        excel_data.to_csv(data_path, mode='a', header=False, index=False)
+        
+    # from datetime import datetime
 
-excel_data.to_csv(f'word_data\\word_data_{today}.csv', index=False)
+    # # 현재 날짜 및 시간 가져와 데이터 생성
+    # today = datetime.now().strftime('%Y-%m-%d')
+
+    # excel_data.to_csv(f'word_data\\word_data_{today}.csv', index=False)
+    print("end")
---
+++ word_test/read_data.py
@@ -0,0 +1,68 @@
+#csv파일을 읽음
+import pandas as pd
+from datetime import datetime
+from sys import stdin
+
+# n, k = 30, 1
+def word_test(n, k):
+    data_path = f'C:\sin\word_memorize\word_data\end_word\word_data.csv'
+    existing_data = pd.read_csv(data_path)
+    # print(existing_data.head())
+
+    #정렬하여 인덱스로 찾아가서 해당 부분만 수정하기에 초기 데이터의 인덱스 순서는 불변함!!
+    sorted_data = existing_data.sort_values(by=['value', '날짜'])
+    # print(sorted_data.head())
+
+    calculate_data = sorted_data.head(n*k)
+    # print(calculate_data.shape)
+    # print(calculate_data.head())
+    # print(calculate_data.flags)
+    for count in range(k):
+        for i in range(count*n,(count+1)*n):
+            row = calculate_data.iloc[i]
+            print(f"{row['단어']}의 뜻은?")
+            tmp = stdin.readline().strip()
+            print("\n\n\n")
+            print(row['뜻'])
+
+            print("T,N,F로 맞춘 정도를 표시하시오 : ")
+            tmp = stdin.readline().strip()
+            index = row['인덱스']
+            # print(index)
+            if row['단어'] != existing_data.loc[index-1, '단어']:
+                print("error 발생!")
+                print(f"{row['단어']}인데 {index}에서 {existing_data.loc[index-1, '단어']}가 적용됨!!")
+            if tmp == "T":
+                # print(existing_data.loc[index-1, '단어'], existing_data.iloc[index-1]['단어'])
+                existing_data.loc[index-1, '날짜'] = datetime.now().strftime("%Y-%m-%d") #맞춘 날짜 갱신
+                #후자는 안됨
+                # existing_data.iloc[index-1]['날짜'] = datetime.now().strftime("%Y-%m-%d") #맞춘 날짜 갱신
+                
+                existing_data.loc[index-1, 'value'] += 30
+            elif tmp == "N":
+                existing_data.loc[index-1, 'value'] += 15
+            elif tmp == "F":
+                existing_data.loc[index-1, 'value'] += 5
+                # tmp = stdin.readline().strip() #한번 쳐보면서 암기하기!
+            else:
+                print("일시 중지됨")
+                existing_data.to_csv(data_path, index=False)
+                print(f"{count*n+1}부터 {i+1}번까지 학습 끝")
+                return
+            # print(calculate_data.at[i, '단어']) #.at은 인덱스 기반으로 찾음! #여긴 없어서 에러!
+
+            #A value is trying to be set on a copy of a slice from a DataFrame는 참조로 전체 중 일부를 가져왔기 때문에
+            #수정이 일어날때 일부, 전체 중 어디에 수정해야할 지 몰라서 생기는 오류
+            #EX) df[df['GROUP'] == 1]['VALUE'] = 10시 에러
+            #df.loc[df['GROUP'] == 1, 'VALUE'] = 10 #원본 데이터 수정
+            #copy를 활용해 #일부분 뽑은 데이터 수정
+                
+            # print((datetime.now()-datetime.strptime(calculate_data.iloc[i]['날짜'], "%Y-%m-%d")).days) #이전 학습일자와의 차이
+
+        # existing_data.iloc[:n*k] = calculate_data
+        # print(existing_data.head())
+
+        existing_data.to_csv(data_path, index=False)
+        print(f"{count*n+1}부터 {i+1}번까지 학습 끝")
+    print("test end")
+    # print(calculate_data.head())
